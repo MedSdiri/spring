39babcbd4edcd41db6a2978ae41bbb91cc4023b2
MedSdiri <sdiri.ad@gmail.com>
2022-02-17 19:41:19 -0600

    Created new Module to practice Dependency injection

--- Spring-02-Core-Wiring/src/main/java/com/cydeo/Person.java
+++ Spring-02-Core-Wiring/src/main/java/com/cydeo/Person.java
@@ -2,8 +2,9 @@ package com.cydeo;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
-@Getter @Setter
+@Getter @Setter @ToString
 public class Person {
 
     private String name;
