86872e09e487562601125d03a42b7bf0d9c62d84
MedSdiri <sdiri.ad@gmail.com>
2022-03-01 19:19:25 -0600

    created controller class and methods with @RequestMapping and added home and welcome html files

--- Spring-07-Spring-MVC-Controller/src/main/java/com/med/ControllerApplication.java
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/med/ControllerApplication.java
@@ -1,4 +1,4 @@
-package com.med.spring07springmvccontroller;
+package com.med;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
