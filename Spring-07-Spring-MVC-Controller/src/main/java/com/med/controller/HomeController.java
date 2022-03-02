39e2f728304716c247df18c5227a7b9ee3d56659
MedSdiri <sdiri.ad@gmail.com>
2022-03-01 19:39:31 -0600

    Added welcome html page and tried the default @RequestMapping result

--- Spring-07-Spring-MVC-Controller/src/main/java/com/med/controller/HomeController.java
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/med/controller/HomeController.java
@@ -4,16 +4,22 @@ package com.med.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller //Annotate the class with the @Controller stereotype annotation
 public class HomeController {
-    @RequestMapping("/home")
+
+    @RequestMapping("/home") //User @RequestMapping annotation to associate the action with an HTTP request path
     public String home() {
-        return "home.html";
+        return "home.html"; //Retrun the HTML file name that contains the details we want the browser to display
     }
 
     @RequestMapping("/welcome")
     public String welcome() {
         return "welcome.html";
+    }
+
+    @RequestMapping
+    public String home3() {
+        return "home.html";
     }
 
 
