4f8d61132864a5ec427932470cb84bdad541ac0e
MedSdiri <sdiri.ad@gmail.com>
2022-03-15 19:16:48 -0500

    printed a list of items using Thymeleaf looping through the list using th:each

--- Spring-09-Spring-thymeleaf/src/main/java/com/med/conrtoller/StudentController.java
+++ Spring-09-Spring-thymeleaf/src/main/java/com/med/conrtoller/StudentController.java
@@ -18,5 +18,10 @@ public class StudentController {
         return "student/register";
     }
 
+    @RequestMapping("/welcome")
+    public String info(){
+        return "student/welcome";
+    }
+
 
 }
